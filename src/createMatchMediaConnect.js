a754ef969ed22da9a1bef797a45a1afa2f505d7c
Malte W <wessel@rheinschafe.de>
2016-02-13 13:24:11 +0100

    rename connect wrapper

--- src/createMatchMediaConnect.js
+++ src/createMatchMediaConnect.js
@@ -61,7 +61,7 @@ export default function createMatchMediaConnect(queryMap = {}) {
         }
         return function wrapWithConnect(Component) {
             return createClass({
-                displayName: 'MatchMediaConnect',
+                displayName: 'ConnectMatchMedia',
                 getInitialState() {
                     return pickState(state);
                 },
